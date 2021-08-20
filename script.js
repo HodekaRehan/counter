6ddab37cbdcc48da6f0c29a2cd662a6571dfdc72
ReyHaan <hodekarehan@gmail.com>
2021-08-20 16:09:01 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//Set initial value to zero
+let counter = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		const styles = e.currentTarget.classList;
+
+		if (styles.contains('decrease')) {
+			counter--;
+		} else if (styles.contains('increase')) {
+			counter++;
+		} else {
+			counter = 0;
+		}
+
+		if (counter > 0) {
+			value.style.color = 'green';
+		} else if (counter < 0) {
+			value.style.color = 'red';
+		} else {
+			value.style.color = '#222';
+		}
+
+		value.textContent = counter;
+	});
+});
